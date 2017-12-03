fe0e4d9051ef208ea5249968778d416f8b348a7a
nama1510 <nama1510@gmail.com>
2017-12-03 20:02:31 +0100

    Fixed Waitress Prefab

---
+++ Assets/_Scripts/Enemies/Boss.cs
@@ -0,0 +1,29 @@
+﻿/*
+* Copyright (c) Ignacio Martinez
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+	#region Variables
+
+	#endregion
+
+	#region Unity Methods
+
+	void Start () 
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+	#endregion
+
+	#region Custom Functions
+	#endregion
+}
